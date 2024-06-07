9fcee540371e6bd250f954f25b0bfcdb44114170
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-06-07 15:16:36 +0000

    map method

--- map.js
+++ map.js
@@ -4,17 +4,32 @@
  * command node map.js in the terminal
  */
 
-// Using a for loop
 
+// Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
-
-
-// Simplified w/ map()
-
-
-// Simplfied w/ map() + arrow function
-
+const multByTwo = function (num) {
+  return num * 2;
+  }
+  
+  const mapResults = nums.map(multByTwo);
+  console.log(mapResults);
+  
+  // Simplified w/ map()
+  
+  const simplified = nums.map(function (num) { return num * 2});
+  console.log(simplified);
+  
+  // Simplfied w/ map() + arrow function
+  
+  const arrowSimplified = nums.map(num => num * 2);
+  console.log(arrowSimplified);
 
 // With objects:
 const students = [
@@ -37,3 +52,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
